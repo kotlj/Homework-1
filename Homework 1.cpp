1500451289ba8442d3aade2ea96821a7845af085
crotl_000 <crotl_000@ADMIN>
2022-04-21 21:14:01 +0300

    Добавьте файлы проекта.

---
+++ Homework 1.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Task 1\n";
+	cout << "\t\tApril  2022\n  Mn  Tu  Wn  Th  Fr  St  Su\n                   1   2   3\n"
+		"   4   5   6   7   8   9  10\n  11  12  13  14  15  16  17\n  18  19  20  21  22  23  24\n"
+		"  25  26  27  28  29  30  31\n\n";
+
+	cout << "Task 2\n";
+	cout << "\t\t\t\tDenis H. O. ***** Ksenia H. O.\n\t\t\t\t\t      *\n\t\t\t\t\t      *\n\t\t\t\t\t      *\n"
+		"\t\t\t\t\t     * *\n\t\t\t\t\t    *   *\n\t\t\t\t           *     *\n\t\t\t\t          *       *\n"
+		"\t\tRita H. O. ***** Olecsand H. O.  Tatyana R. B. ***** Galya R. B.\n"
+		"\t\t\t     *\t\t\t\t\t *\n\t\t\t     *\t\t\t\t\t *"
+		"\n\t\t\t     *\t\t\t\t\t *\n\t\t\t     *\t\t\t\t\t *\n"
+		"\t\t\t    * *\t\t\t                * *\n\t\t\t  *   *"
+		"\t\t\t                *   *\n\t\t\t*     *\t\t\t                *     *\n"
+		"\t\tGrandfather  Grandmother\t\tGrandfather  Grandmother\n\n";
+
+	// Im tired, so next two will be Ctrl + mouse
+	cout << "Task 3\n";
+	cout << "|----------------------------------------------------------------------------|\n";
+	cout << "|                                                                            |\n";
+	cout << "| ****         ***************  *****        *** ******         **********   |\n";
+	cout << "| ******       ***************  *** **       *** ******       *************  |\n";
+	cout << "| *** ***      ***              *** **       ***  ****       **********      |\n";
+	cout << "| ***   ***    ***              ***  **      ***  ****       *********       |\n";
+	cout << "| ***    ***   ***              ***  **      ***   **        *******         |\n";
+	cout << "| ***     ***  ***              ***   **     ***   **        *******         |\n";
+	cout << "| ***      *** ***              ***   **     ***   **         ********       |\n";
+	cout << "| ***      *** ***************  ***    **    ***   **          *********     |\n";
+	cout << "| ***      *** ***************  ***    **    ***   **            *********   |\n";
+	cout << "| ***     ***  ***              ***     **   ***   **	            ********  |\n";
+	cout << "| ***    ***   ***              ***     **   ***   **	             *******  |\n";
+	cout << "| ***   ***    ***              ***      **  ***   **	             *******  |\n";
+	cout << "| ***  ***     ***              ***      **  ***  ****	            ********  |\n";
+	cout << "| *** ***      ***              ***       ** ***  ****           *********   |\n";
+	cout << "| ******       ***************  ***       ** *** ******      ************    |\n";
+	cout << "| ****         ***************  ***        ***** ******       **********     |\n";
+	cout << "|                                                                            |\n";
+	cout << "|----------------------------------------------------------------------------|\n\n";
+
+	cout << "Task 4\n";
+	cout <<"|------------------------------------------------------------------------------|";
+	cout <<"|                                       SUMMARY                                |";
+	cout <<"|                                                                              |";
+	cout <<"|                              Denis Hutsul Olexandrovitch                     |";
+	cout <<"|                                                                              |";
+	cout <<"|                                                                              |";
+	cout <<"|    Work Expiriance:                                                          |";
+	cout <<"|                                                                              |";
+	cout <<"|                                                                              |";
+	cout <<"|         None                                                                 |";
+	cout <<"|                                                                              |";
+	cout <<"|                                                                              |";
+	cout <<"|    Study:                                                                    |";
+	cout <<"|                                                                              |";
+	cout <<"|    IT Step Academy                                                           |";
+	cout <<"|    Ukraine, Odessa                                                           |";
+	cout <<"|    Programing on diferend program languages                                  |";
+	cout <<"|    (2021 - ...)                                                              |";
+	cout <<"|                                                                              |";
+	cout <<"|                                                                              |";
+	cout <<"|    Traits:                                                                   |";
+	cout <<"|                                                                              |";
+	cout <<"|    Not communicable, hardworker,                                             |";
+	cout <<"|    always online                                                             |";
+	cout <<"|                                                                              |";
+	cout <<"|    Recomendati:                                                              |";
+	cout <<"|    None                                                                      |";
+	cout <<"|                                                                              |";
+	cout <<"|------------------------------------------------------------------------------|\n\n\n";
+}
